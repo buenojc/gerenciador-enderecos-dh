f5f51798ebe444d5dc1f7adce51dc2785ab2939e
Julio Cesar Bueno <buenojc@outlook.com>
2023-05-03 11:31:04 -0300

    :sparkles: feat novas rotas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
+import NotFoundScreen from "./screens/NotFoundScreen";
 import UserRegisterScreen from "./screens/UserRegisterScreen";
 import UsersScreen from "./screens/UsersScreen";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return(
-    <UsersScreen />
-  )  
+  return (
+    <Routes>
+      <Route element={<UsersScreen />} path="/" />
+      <Route element={<UserRegisterScreen />} path="/registro" />
+      <Route element={<NotFoundScreen />} path="*" />
+    </Routes>
+  );
 }
 
 export default App;
